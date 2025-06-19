9aef1c4e88bb1f58bdc29ccdda3b765d2d0b0435
Rishi Mishra <mishrar1880@gmail.com>
2025-06-19 16:09:22 +0530

    Initial build

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import HomePage1 from "./(homes)/home-01/page";
+
+export const metadata = {
+  title: "Home 1 || The Upthrust",
+  description: "The Upthrust- Boosting tech competence",
+};
+export default function Home() {
+  return (
+    <>
+      <HomePage1 />
+    </>
+  );
+}
